b442be336454c07318e3b08b0b4c9aa9c7338712
beomju <20171643.info@edu.hanbat.ac.kr>
2020-10-11 23:16:09 +0900

    Add nunjucks

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
+const path = require('path');
+const nunjucks = require('nunjucks');
 
 indexRouter = require('./routes/index');
 const {sequelize} = require('./models');
 
 const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
 
 app.set('port', process.env.PORT || 4000);
 sequelize.sync({force: false})
@@ -20,6 +28,8 @@ sequelize.sync({force: false})
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.use(morgan('dev'));
